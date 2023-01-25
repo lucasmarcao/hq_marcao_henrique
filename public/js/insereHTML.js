c8a2c418c9ecd6b6dc6c3f484861ed5e7d32d501
Lucas Marcão <69424024+lucasmarcao@users.noreply.github.com>
2023-01-25 11:17:37 -0300

    main: deploy in heroku

--- public/js/insereHTML.js
+++ public/js/insereHTML.js
@@ -25,8 +25,6 @@ try {
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/form.css">
     <link rel="stylesheet" href="/css/hq.css">
-    <link rel="stylesheet" href="/css/global.css">
-    <link rel="stylesheet" href="/css/nav.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
