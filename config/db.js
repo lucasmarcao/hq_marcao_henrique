ae08511c1cab720112b541dc37226ce4085bd356
Lucas Marcão <69424024+lucasmarcao@users.noreply.github.com>
2023-01-26 16:14:59 -0300

    marcao: botando o handlebars no deploy

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-  module.exports = { MongoURI: "mongodb://localhost/blogmarcao" }
+  module.exports = { MongoURI: "mongodb://localhost/hq_marcao_henrique" }
 } else {
   module.exports = { MongoURI: 
     "mongodb+srv://marcaozitos:enquebravel1@cluster0.zub3f.mongodb.net/test" }
